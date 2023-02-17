36d586ef67e0d207d2e84c8e982334d6932fc117
Xusy1031 <sxu7@uw.edu>
2023-02-17 00:03:26 -0800

    finish chart 3

---
+++ Chart 3.R
@@ -0,0 +1,36 @@
+original_df <- read.csv("mxmh_survey_results.csv", stringsAsFactors = FALSE)
+
+library("dplyr")
+library("tidyverse")
+library("ggplot2")
+library("reshape2")
+
+music_effect_frequency <- original_df %>% 
+  filter(Music.effects == "Improve") %>% 
+  select(Frequency..Classical., Frequency..Country., Frequency..EDM., Frequency..Folk., Frequency..Gospel.,
+         Frequency..Hip.hop., Frequency..Jazz., Frequency..K.pop., Frequency..Latin., Frequency..Lofi., 
+         Frequency..Metal., Frequency..Pop., Frequency..R.B., Frequency..Rap., Frequency..Rock.,
+         Frequency..Video.game.music., Music.effects) %>% 
+  select(-Music.effects)
+
+mef_df <- data.frame(sapply(X = music_effect_frequency, FUN = table))
+
+colnames(mef_df) <- c("Classical", "Country", "EDM", "Folk", "Gospel", "HipHop", "Jazz", "Kpop", "Latin", "Lofi", "Metal",
+                      "Pop", "Rap", "R&B", "Rock", "Video Game")
+
+mef_df <- as.data.frame(t(mef_df))
+
+mef_df <- mef_df %>% 
+  mutate(Music_Genre = c("Classical", "Country", "EDM", "Folk", "Gospel", "HipHop", "Jazz", "Kpop", "Latin", "Lofi", "Metal",
+           "Pop", "Rap", "R&B", "Rock", "Video Game"))
+  
+new_mef_df <- melt(mef_df, id.vars = "Music_Genre")
+
+ggplot(data = new_mef_df) +
+  geom_bar(aes(x = Music_Genre, y = value, fill = variable), stat = "identity", position = "stack") +
+  scale_fill_brewer(palette = "Pastel2") +
+  scale_y_continuous(breaks = c(50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550)) +
+  labs(title = "Music Genre vs Music Effects",
+       x = "Music Genre",
+       y = "Number of People Who Report 'Improve'",
+       fill = "Frequency") 
